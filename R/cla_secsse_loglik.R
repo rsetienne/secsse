b80540582e1233f5369ddc661529bb47471ad699
leonelhalsina <leonelhalsina@gmail.com>
2019-05-30 15:11:50 +0200

    I deleted a line to print the outcome from integration

--- R/cla_secsse_loglik.R
+++ R/cla_secsse_loglik.R
@@ -71,6 +71,7 @@ cla_calThruNodes <- function(
     if(desIndex == 2){
       nodeM <- nodeMN 
     }
+    #print(nodeMN)     ########################
   }
   ## At the node
   nodeM <- as.numeric(nodeM[2,-1])
