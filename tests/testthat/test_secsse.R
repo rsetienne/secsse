5d6a397e28e518f7dcf398ceff9faaff6729a4d8
= <=>
2021-02-11 09:02:07 +0100

    spaces

--- tests/testthat/test_secsse.R
+++ tests/testthat/test_secsse.R
@@ -239,8 +239,8 @@ test_that("trying a short ML search: secsse_ml & parallel procedure", {
   
   Sys.unsetenv("R_TESTS")
   parenthesis <- "(((6:0.2547423371,(1:0.0496153503,4:0.0496153503):0.2051269868):0.1306304758,(9:0.2124135406,5:0.2124135406):0.1729592723):1.151205247,(((7:0.009347664296,3:0.009347664296):0.2101416075,10:0.2194892718):0.1035186448,(2:0.2575886319,8:0.2575886319):0.06541928469):1.213570144);"
-  phylotree<-ape::read.tree(file="",parenthesis)
-  traits <-  c(2, 0, 1, 0, 2, 0, 1, 2, 2, 0)
+  phylotree <- ape::read.tree(file="",parenthesis)
+  traits <- c(2, 0, 1, 0, 2, 0, 1, 2, 2, 0)
   num_concealed_states <- 3
   idparslist <- id_paramPos(traits, num_concealed_states)
   idparslist[[1]][c(1,4,7)] <- 1
@@ -258,13 +258,13 @@ test_that("trying a short ML search: secsse_ml & parallel procedure", {
   initparsopt <- c(rep(intGuessLamba,3))
   idparsfix <- c(0,4,5)
   parsfix <- c(0,0,0.1)
-  tol = c(1e-04, 1e-05, 1e-07)
-  maxiter = 1000 * round((1.25)^length(idparsopt))
-  use_fortran = TRUE
-  methode = "ode45"
-  optimmethod = "simplex"
-  run_parallel = TRUE
-  cond<-"proper_cond"
+  tol <- c(1e-04, 1e-05, 1e-07)
+  maxiter <- 1000 * round((1.25)^length(idparsopt))
+  use_fortran <- TRUE
+  methode <- "ode45"
+  optimmethod <- "simplex"
+  run_parallel <- TRUE
+  cond <- "proper_cond"
   root_state_weight <- "proper_weights"
   sampling_fraction <- c(1,1,1)
   model <- secsse_ml(
