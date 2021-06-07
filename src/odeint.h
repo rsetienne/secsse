f877a4e277cf6b7ec591ba675c350d7e0400bff1
Thijs Janzen <thijsjanzen@gmail.com>
2021-06-07 16:17:27 +0200

    add E part

--- src/odeint.h
+++ src/odeint.h
@@ -226,11 +226,19 @@ public:
                 std::vector< double > &dxdt,
                 const double /* t */ ) const {
     
-   std::vector<double> dC(d, 0);
+   
    for (int i = 0; i < d; ++i) {
-     dxdt[i + d] = - (lambda_sum[i] + m_[i]) * x[i + d];
+     
+     dxdt[i] = m_[i] * (1.0 - x[i]);
+     dxdt[i + d] = -1.0 * (lambda_sum[i] + m_[i]) * x[i + d];
+     
      for (int j = 0; j < d; ++j) {
+      dxdt[i]     += q_[i][j] * (x[j] - x[i]);
       dxdt[i + d] += q_[i][j] * (x[j + d] - x[i + d]);
+      
+      for (int k = 0; k < d; ++k) {
+        dxdt[i] += l_[i][j][k] * (x[j] * x[k] - x[i]);
+      }
      }
    }
   }
