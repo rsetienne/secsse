ad737aba3597bf5ce5352e439b60132a47475b03
Neves-P <p.m.santos.neves@rug.nl>
2023-06-27 17:02:29 +0200

    Storep plots in variable in examples

--- R/plot_state_exact_cla.R
+++ R/plot_state_exact_cla.R
@@ -78,16 +78,16 @@
 #'helper_function <- function(x) {
 #'  return(sum(x[c(10, 13, 16)]) / sum(x))
 #'}
-#'plot_state_exact_cla(parameters = parameter,
-#'                             focal_tree = phy,
-#'                             traits = traits,
-#'                             num_concealed_states = 3,
-#'                             sampling_fraction = sampling_fraction,
-#'                             cond = 'maddison_cond',
-#'                             root_state_weight = 'maddison_weights',
-#'                             is_complete_tree = FALSE,
-#'                             prob_func = helper_function,
-#'                             steps = 10)
+#'out_plot <- plot_state_exact_cla(parameters = parameter,
+#'                                 focal_tree = phy,
+#'                                 traits = traits,
+#'                                 num_concealed_states = 3,
+#'                                 sampling_fraction = sampling_fraction,
+#'                                 cond = 'maddison_cond',
+#'                                 root_state_weight = 'maddison_weights',
+#'                                 is_complete_tree = FALSE,
+#'                                 prob_func = helper_function,
+#'                                 steps = 10)
 #' @export
 plot_state_exact_cla <- function(parameters,
                                  focal_tree,
