079bce20b05ee65b9ee399f24170bdbe8e4afad5
Hanno Hildenbrandt <h.hildenbrandt@rug.nl>
2023-07-11 15:38:40 +0200

    flat Q

--- src/secsse_rhs.h
+++ src/secsse_rhs.h
@@ -60,17 +60,32 @@ namespace secsse {
   };
 
 
+  inline auto flat_q_matrix(const Rcpp::NumericMatrix& rq) {
+    assert(rq.nrow() == rq.ncol());
+    const auto d = static_cast<size_t>(rq.nrow());
+    auto q = std::vector<double>(d * d);
+    auto qv = vector_view_t<double>{q.data(), d};
+    for (auto i = 0; i < d; ++i, qv.advance(d)) {
+      auto qrow = rq.row(i);
+      for (auto j = 0; j < d; ++j) {
+        qv[j] = qrow[j];
+      }
+    }
+    return q;
+  }
+
+
   template <OdeVariant variant>
   class ode_standard {
     const_rvector<double> l_;
     const_rvector<double> m_;
-    const_rmatrix<double> q_;
+    const std::vector<double> q_;
 
   public:
     ode_standard(const Rcpp::NumericVector& l,
                  const Rcpp::NumericVector& m,
                  const Rcpp::NumericMatrix& q)
-    : l_(l), m_(m), q_(q) {
+    : l_(l), m_(m), q_(flat_q_matrix(q)) {
     }
 
     size_t size() const noexcept { return l_.size(); }
@@ -91,15 +106,15 @@ namespace secsse {
       const auto d = size();
       if  constexpr (variant == OdeVariant::normal_tree) {
         // normal tree
-        for (size_t i = 0; i < d; ++i) {
+        auto qv = vector_view_t<const double>{q_.data(), d};
+        for (size_t i = 0; i < d; ++i, qv.advance(d)) {
           const double t0 = l_[i] + m_[i];
           const double t1 = l_[i] * x[i];
           double dx0 = m_[i] + (t1 - t0) * x[i];
           double dxd = (2 * t1 - t0) * x[i + d];
-          auto q = q_.row(i);
           for (size_t j = 0; j < d; ++j) {
-            dx0 += (x[j] - x[i]) * q[j];
-            dxd += (x[j + d] - x[i + d]) * q[j];
+            dx0 += (x[j] - x[i]) * qv[j];
+            dxd += (x[j + d] - x[i + d]) * qv[j];
           }
           dxdt[i] = dx0;
           dxdt[i + d] = dxd;
@@ -107,13 +122,13 @@ namespace secsse {
       }
       else if constexpr ((variant == OdeVariant::complete_tree) || (variant == OdeVariant::ct_condition)) {
         // complete tree including extinct branches or conditioning
-        for (size_t i = 0; i < d; ++i) {
+        auto qv = vector_view_t<const double>{q_.data(), d};
+        for (size_t i = 0; i < d; ++i, qv.advance(d)) {
           double dx0 = (m_[i] - (l_[i] * x[i])) * (1 - x[i]);
           double dxd = -(l_[i] + m_[i]) * x[i + d];
-          auto q = q_.row(i);
           for (size_t j = 0; j < d; ++j) {
-            dx0 += (x[j] - x[i]) * q[j];
-            dxd += (x[j + d] - x[i + d]) * q[j];
+            dx0 += (x[j] - x[i]) * qv[j];
+            dxd += (x[j + d] - x[i + d]) * qv[j];
           }
           dxdt[i] = dx0;
           dxdt[i + d] = dxd;
@@ -123,12 +138,12 @@ namespace secsse {
   };
 
 
-  struct cla_precomp_t {
-    std::vector<double> ll;     // flattened ll matrices
+  struct ode_cla_precomp_t {
+    std::vector<double> ll;     // flat, transposed ll matrices
     std::vector<std::vector<size_t>> nz;
     std::vector<double> lambda_sum;
 
-    explicit cla_precomp_t(const Rcpp::List& Rll) {
+    explicit ode_cla_precomp_t(const Rcpp::List& Rll) {
       const auto n = Rll.size();
       auto probe = Rcpp::as<Rcpp::NumericMatrix>(Rll[0]);
       assert(probe.nrow() == probe.ncol());
@@ -158,14 +173,14 @@ namespace secsse {
   class ode_cla {
     // used for normal tree
     const const_rvector<double> m_;
-    const const_rmatrix<double> q_;
-    const cla_precomp_t prec_;
+    const std::vector<double> q_;   // flat, transposed q matrix
+    const ode_cla_precomp_t prec_;
 
   public:
     ode_cla(const Rcpp::List ll,
             const Rcpp::NumericVector& m,
             const Rcpp::NumericMatrix& q)
-    : m_(m), q_(q), prec_(ll) {
+    : m_(m), q_(flat_q_matrix(q)), prec_(ll) {
     }
 
     size_t size() const noexcept { return m_.size(); }
@@ -194,17 +209,17 @@ namespace secsse {
       if constexpr (variant == OdeVariant::normal_tree) {
         auto llv = vector_view_t<const double>(prec_.ll.data(), d);
         auto nzv = prec_.nz.begin();
-        for (size_t i = 0; i < d; ++i) {
+        auto qv = vector_view_t<const double>{q_.data(), d};
+        for (size_t i = 0; i < d; ++i, qv.advance(d)) {
           double dx0 = 0.0;
           double dxd = 0.0;
-          auto q = q_.row(i);
           for (size_t j = 0; j < d; ++j, llv.advance(d), ++nzv) {
             for (auto k : *nzv) {
               dx0 += llv[k] * (x[j] * x[k]);
               dxd += llv[k] * (x[j] * x[k + d] + x[j + d] * x[k]);
             }
-            dx0 += (x[j] - x[i]) * q[j];
-            dxd += (x[j + d] - x[i + d]) * q[j];
+            dx0 += (x[j] - x[i]) * qv[j];
+            dxd += (x[j + d] - x[i + d]) * qv[j];
           }
           dxdt[i] = dx0 + m_[i] - (prec_.lambda_sum[i] + m_[i]) * x[i];
           dxdt[i + d] = dxd - (prec_.lambda_sum[i] + m_[i]) * x[i + d];
@@ -212,11 +227,11 @@ namespace secsse {
       }
       else if constexpr (variant == OdeVariant::complete_tree) {
         // complete tree including extinct branches
-        for (size_t i = 0; i < d; ++i) {
+        auto qv = vector_view_t<const double>{q_.data(), d};
+        for (size_t i = 0; i < d; ++i, qv.advance(d)) {
           double dxd = -(prec_.lambda_sum[i] + m_[i]) * x[i + d];
-          auto q = q_.row(i);
           for (size_t j = 0; j < d; ++j) {
-            dxd += (x[j + d] - x[i + d]) * q[j];
+            dxd += (x[j + d] - x[i + d]) * qv[j];
           }
           dxdt[i + d] = dxd;
         }
@@ -224,11 +239,11 @@ namespace secsse {
       else if constexpr (variant == OdeVariant::ct_condition) {
         auto llv = vector_view_t<const double>(prec_.ll.data(), d);
         auto nzv = prec_.nz.begin();
-        for (size_t i = 0; i < d; ++i) {
+        auto qv = vector_view_t<const double>{q_.data(), d};
+        for (size_t i = 0; i < d; ++i, qv.advance(d)) {
           double dx0 = m_[i] * (1 - x[i]);
-          auto q = q_.row(i);
           for (size_t j = 0; j < d; ++j, llv.advance(d), ++nzv) {
-            dx0 += (x[j] - x[i]) * q[j];
+            dx0 += (x[j] - x[i]) * qv[j];
             for (auto k : *nzv) {
               dx0 += llv[k] * (x[j] * x[k] - x[i]);
             }
